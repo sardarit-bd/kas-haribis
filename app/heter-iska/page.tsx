e7cf79fa556412ab2d2567b8626ef64bb6ed2288
sardarit-bd <236187923+sardarit-bd@users.noreply.github.com>
2026-09-24 14:29:44 +0600

    in heter-iska page added direction in the one paragraph or p tag

--- app/heter-iska/page.tsx
+++ app/heter-iska/page.tsx
@@ -55,7 +55,7 @@ export default function HeterIska() {
         <p className="text-base lg:text-2xl font-semibold max-w-5xl mx-auto">
           Get a rabbinically approved Heter Iska template to use for your business or personal loan arrangements.
         </p>
-        <p className="text-sm lg:text-lg mt-5 max-w-7xl mx-auto">
+        <p dir="rtl" className="text-sm lg:text-lg mt-5 max-w-7xl mx-auto">
           וזה לשון היערות דבש (דף קכד) "וכבר מצאו חז"ל נוחי נפש תקנה בעשיית שטר עיסקא אבל צריך להזהר בו ולעשות הכל כדינו כי רבו דיניה ויקצר הזמן והיריעה לדורשו ברבים, אבל מ"מ מי האיש החפץ חיים ולקום בתחיית המתים ישאל פי חכם בעשותו הלואה כזו
         </p>
       </div>
